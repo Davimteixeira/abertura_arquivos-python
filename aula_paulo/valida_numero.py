5eb26cc7a291df5ee6939e6eb56c2a4282467bd4
Davimteixeira <daviteixeira077@gmail.com>
2023-01-24 18:38:34 -0300

    abertura de arquivos python

--- aula_paulo/valida_numero.py
+++ aula_paulo/valida_numero.py
@@ -1,3 +1,9 @@
+# numeros pares digitados
 numero = [int(input('informe um numero: '))]
 numero_par = list(filter(lambda x: x %2 == 0, numero))
 print(numero_par)
+
+# numeros impar em lista
+numero = [1,2,3,4,5,6,7,8,9,10]
+numero_impar = list(filter(lambda x: x %2 == 1, numero))
+print(numero_impar)
